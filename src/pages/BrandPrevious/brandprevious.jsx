0dc1ae02dbef70bd27ac581c7527a76ff502df08
itsmdshahin <mdshahinurrahman8@gmail.com>
2023-11-01 00:08:10 +0600

    itsmdshahin

--- src/pages/BrandPrevious/brandprevious.jsx
+++ src/pages/BrandPrevious/brandprevious.jsx
@@ -1,44 +1,245 @@
 import React from 'react'
 import { Footer } from "../../components/headerPage/footer";
 import Header from "../../components/headerPage/header";
+import "./brandprevious.scss";
+import { FaAngleDown } from 'react-icons/fa';
+
 const BrandPrevious = () => {
   return (
     <>
       <Header />
+
       <div className='brandprevious'>
-      <div class="grid">
-  <div class="grid__item">
-    <div class="card">
-        <img class="card__img" src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80" alt="Snowy Mountains"/>
-      <div class="card__content">
-        <h1 class="card__header">A starry night</h1>
-        <p class="card__text">Look up at the night sky, and find yourself <strong>immersed</strong> in the amazing mountain range of Aspen. </p>
-        <button class="card__btn">Explore <span>&rarr;</span></button>
+        {/* This is Car showing strating  */}
+        <div className='thirddiv'>
+          <h2 className='thirddivh3'>Best Deals First <a href=""><FaAngleDown className='logo' /></a></h2>
+ 
+          <div className='thirdone'>
+            <div className="thirdtwo">
+              <img src="../../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+           {/* this is one car listing showing  */}
+
+            <div className="thirdtwo">
+              <img src="../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            
+
+            <div className="thirdtwo">
+              <img src="../../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div className="thirdtwo">
+              <img src="../../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div className="thirdtwo">
+              <img src="../../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div className="thirdtwo">
+              <img src="../../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div className="thirdtwo">
+              <img src="../../../../images/ca6.jpg" alt="Car" className='thirdimg' />
+              <div className="thirdthree">
+                <h3>2015 Marcedes-Benz Sprinter Cab</h3>
+
+                <div className='thirdfour'>
+                  <div className='thirdfourone'>
+                    <p className='thirdp'>$25,500</p>
+                    <p className='thirdp'>163,383 mile</p>
+                    <p className='thirdp'>Clarksville,MD</p>
+                    <p className='thirdpp'>(880)1234567</p>
+                  </div>
+
+                  <div className='thirdfourtwo'>
+                    <p>$9,730 below market</p>
+                  </div>
+                </div>
+
+                <div className='thirdfive'>
+                  <div className='thirdfiveone'>
+                    <button className="btn2">Request Info</button>
+                  </div>
+                  <div className='thirdfiveone'>
+                    <button className="btn">Start Purchasing</button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </div>
-    </div>
-  </div>
-  <div class="grid__item">
-    <div class="card">
-        <img class="card__img" src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80" alt="Desert"/>
-      <div class="card__content">
-        <h1 class="card__header">Misty mornings</h1>
-        <p class="card__text">Capture the stunning <strong>essence</strong> of the early morning sunrise in the Californian wilderness.</p>
-        <button class="card__btn">Explore <span>&rarr;</span></button>
-      </div>
-    </div>
-  </div>
-  <div class="grid__item">
-    <div class="card">
-        <img class="card__img" src="https://images.unsplash.com/photo-1506318164473-2dfd3ede3623?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=3300&amp;q=80" alt="Canyons"/>
-      <div class="card__content">
-        <h1 class="card__header">Utah sunsets</h1>
-        <p class="card__text">Sunsets over the <strong>stunning</strong> Utah Canyonlands, is truly something much more than incredible.</p>
-        <button class="card__btn">Explore <span>&rarr;</span></button>
-      </div>
-    </div>
-  </div>
-</div>
-      </div>
+
       <Footer />
     </>
   )
--- src/pages/userprofile/UserProfile.jsx
+++ src/pages/userprofile/UserProfile.jsx
@@ -1,64 +1,17 @@
-import React, { useEffect, useState } from "react";
 import { Footer } from "../../components/headerPage/footer";
 import Header from "../../components/headerPage/header";
 import Profilee from "../Profilee/Profilee";
- 
 
 
 const UserProfile = () => {
 
-    const [users, setUsers] = useState([]);
-    const [currentUser, setCurrentUser] = useState(null);
-    
-    useEffect(() => {
-        fetch('http://localhost:5000/api/users', {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}` // Include the JWT token
-          }
-        })
-          .then(response => response.json())
-          .then(data => {
-            setUsers(data);
-      
-            // Find the logged-in user based on the stored user ID
-            const loggedInUserId = localStorage.getItem('userId');
-            const loggedInUser = data.find(user => user._id === loggedInUserId);
-            setCurrentUser(loggedInUser);
-          })
-          .catch(error => {
-            console.log('Error fetching data ' + error);
-          });
-      }, []);
-
-
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/api/users', {
-    //         headers: {
-    //             Authorization: `Bearer ${localStorage.getItem('token')}` // Include the JWT token
-    //         }
-    //     })
-    //     .then(response => response.json())
-    //     .then(data => setUsers(data))
-
-
-
-    //     .catch(error => {
-    //         console.log('Error fetching data ' + error);
-    //     }); 
-    
-    // }, []);
-
-   
-
-    
-     return (
-
-        <>
-            <Header />  
-              <Profilee />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Profilee />
+      <Footer />
+    </>
+  );
 }
 
 export default UserProfile;
