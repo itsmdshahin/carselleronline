a576960348b902e08a7dcda23073bc180a5286ba
itsmdshahin <mdshahinurrahman8@gmail.com>
2023-12-06 11:46:46 +0600

    update by itsmdshahin

--- src/pages/PreviewPage/CarPreview/CarPreview.jsx
+++ src/pages/PreviewPage/CarPreview/CarPreview.jsx
@@ -1,9 +1,47 @@
 // import { Button } from 'bootstrap'
 import './carpreview.scss'
 import PROFILEPIC from '../../../assets/usersprofileimg/itsmdshahinpro.png';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const CarPreview = () => {
+
+    const { carId } = useParams();
+    console.log(carId);
+
+    const [carData, setCarData] = useState({});
+    const [userIdInfo, setUserIdInfo] = useState({});
+    useEffect(() => {
+        // Fetch the car details based on the id using axios
+        axios.get(`http://localhost:5000/api/getcalldatalisting/${carId}`)
+            .then((response) => {
+                console.log(response.data, "CarData");
+                setCarData(response.data.carProfile); // Assuming name is in response.data
+                const userId = response.data.carProfile.userId;
+                // Make a new API request to get user details
+                axios.get(`http://localhost:5000/profile/${userId}`)
+                    .then((userResponse) => {
+                        // Assuming user name is in userResponse.data
+                        console.log(userResponse.data, "UserData");
+                        // Update state with user details
+                        setUserIdInfo(userResponse.data);
+                        console.log("ALL DATE FOUND USER!" + userResponse.data);
+                    })
+                    .catch((userError) => {
+                        console.error('Error fetching user data:', userError);
+                        // Handle error, e.g., show a user-friendly message
+                    });
+
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                // Handle error, e.g., show a user-friendly message
+            });
+    }, [carId]); // Include id in the dependency// Include id in the dependency array to refetch data when id changes
+
+    console.log("This is :" + carData);
+
     return (
         <>
             <div className='carpreview'>
@@ -27,7 +65,7 @@ const CarPreview = () => {
                 <div className="headeradmin">
                     <img width='100px' src={PROFILEPIC} alt="profile" />
                     <div className="headerdes">
-                        <h3>Md Shahinur Rahman</h3>
+                        <h3>{userIdInfo.name}</h3>
                         <h4>Sylhet, Bangladesh</h4>
                     </div>
                 </div>
